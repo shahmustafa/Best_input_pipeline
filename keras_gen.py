1edf94e3878350a3cacd213f768b73f2773c3b52
appy1 <mustafa@appynitty.com>
2023-10-28 15:09:19 +0530

    initial commit

--- keras_gen.py
+++ keras_gen.py
@@ -19,7 +19,7 @@ class TimeHistory(tf.keras.callbacks.Callback):
 path = 'WasteImagesDataset'
 IMG_SIZE = 224
 batch_size = 32
-epochs = 5
+epochs = 2
 
 # with mlflow.start_run():
 #     mlflow.tensorflow.autolog()
@@ -32,11 +32,12 @@ model = build_model(num_classes)
 
 time_callback = TimeHistory()
 model.fit(dataset, epochs=epochs, batch_size=32, verbose=2, callbacks=[time_callback])
-
+_, acc = model.evaluate(dataset, verbose=0)
 exp_name = "input_pipeline"
 mlflow.set_experiment(exp_name)
 # Log the epoch times in MLflow
-with mlflow.start_run(run_name='keras_gen'):
+with mlflow.start_run(run_name='keras_gen2'):
     for epoch, epoch_time in enumerate(time_callback.times):
         mlflow.log_param('epoch', epochs)
         mlflow.log_metric(f'epoch_{epoch}_time', epoch_time)
+        mlflow.log_metric('accuracy', acc)
